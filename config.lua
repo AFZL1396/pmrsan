8c1e96a819e7593da049a8d4d49970f620b75a63
AFZL1396 <abol.anvari@gmail.com>
2017-04-18 14:23:21 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
-bot_token = "245451534:AAEgrx-zGY_wRWkR0qYHi2xjSNL-qEjMZFU"
+bot_token = 366275514:AAGxGNzjKATPGuzf7OHwEL7q9P8jHrpn0-8
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "6.0"
-sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 259300954
-admingp = -135847900
-sudo_num = "989351372038"
-sudo_user = "shayan_soft"
-sudo_ch = "UmbrellaTeam"
+sudo_name = "✪ŠυÐΘ™šτરιкє✪"
+sudo_id = 277313212
+admingp = -190837356
+sudo_num = "9890395....."
+sudo_user = "SuDoTMstRiKe"
+sudo_ch = "TeleStRiKe"
